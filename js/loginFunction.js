a95efffb4806f704f8eb48cac3621146a591c5c6
rishikajain-26 <rshikajain2006@gmail.com>
2025-07-13 12:30:36 +0530

    Made changes in the code for cleanup and improvements

--- js/loginFunction.js
+++ js/loginFunction.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (passwordToggle) {
     passwordToggle.addEventListener('click', () => {
-      // Check the current type of the password input
+      
       const type =
         passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
       passwordInput.setAttribute('type', type);
 
-      // Change the icon based on the type
+      
       passwordToggle.textContent = type === 'password' ? '👁️' : '🙈';
     });
   }
